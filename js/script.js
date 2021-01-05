ec29c34de3b090a7ec75f29380ca3fd2f92f47a1
Vadym Babik <vadym.babik@gmail.com>
2021-01-05 20:47:22 +0200

    burger_button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 "use strict";
+document.querySelector(".menu").onclick = () => {
+  document.querySelector(".menu").classList.toggle("active");
+};
+
+//
+// "use strict";
+// document.querySelector(".menu").onclick = function () {
+//   open();
+// };
+//
+// function open() {
+//   document.querySelector(".menu").classList.toggle("active");
+// }
