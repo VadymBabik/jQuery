0145b2549260412f20bacbcc0789cf95fe5f10f4
Vadym Babik <vadym.babik@gmail.com>
2021-01-12 01:49:14 +0200

    rise_top

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+//
+$(function () {
+  let btn = $(".btn-top");
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > $(window).height()) {
+      btn.show(100);
+    } else {
+      btn.hide(100);
+    }
+  });
+  btn.on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "200");
+  });
+});
